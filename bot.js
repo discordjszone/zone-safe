c90f6622054391a5c181f44dfc8194a187ea333e
discordjszone <54225918+discordjszone@users.noreply.github.com>
2019-08-29 03:06:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const giphy = require('giphy-api')();
 const googl = require('goo.gl'); 
 const translate = require('google-translate-api'); 
 const fs = require("fs");
+const ms = require('ms');
 const getYoutubeID = require('get-youtube-id'); 
 const moment = require("moment");
 const { Client, Util } = require('discord.js');
